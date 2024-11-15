28106957ed4a42a161a06cadc72475a939584560
EliasASoovoroff <elias.a.soovoroff@student.uts.edu.au>
2024-11-16 02:16:06 +1100

    Add HUD with Lives, Score, Timer, Scared, Exit

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,11 @@ public class SceneLoader : MonoBehaviour
 {
 	public static void LoadStartScene()
 	{
+		SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+	}
+
+	public static void LoadGameScene()
+	{
 		SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
 	}
 }
